87b7d962468b1fe2072bc3a2eb0a5c4c735509a2
Kien Luu <kienluu@kien56.local>
2013-01-10 22:11:01 +0000

    added music notes and a js file to generate noets

--- webapp/js/main.js
+++ webapp/js/main.js
@@ -9,4 +9,36 @@
         $('.start-button').click();
     });
 
+    var RandomNoteTrainer = function () {
+        var letters = ['a', 'a#', 'b', 'c', 'c#', 'd', 'd#', 'e', 'f', 'f#', 'g', 'g#'];
+        var octaves = ['2','3'];
+        var urlPrefix = '/mp3/note_';
+        var extension = '.mp3';
+        var notes = [];
+        var noteA;
+        var noteB;
+
+        var init = function () {
+            for (var i=0; i < octaves.length; i++){
+                var octave = octaves[i];
+                for (var j=0; j < letters.length; j++){
+                    notes.push({
+                       url: encodeURI(urlPrefix + letters[j].replace('#','sharp') + octave + extension),
+                       name: letters[j]
+                    });
+                }
+            }
+        };
+
+        this.randomNotes = function() {
+            noteA = notes[Math.floor(Math.random() * notes.length)];
+            noteB = notes[Math.floor(Math.random() * notes.length)];
+        };
+
+        this.getNoteA = function() {return noteA; }
+        this.getNoteB = function() {return noteB; }
+
+        init();
+    }
+    debugger;
 })(window.jQuery);
