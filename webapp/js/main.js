ba15dcd48652306e7d80ddd1d79efa54acc9ad9b
Kien Luu <kienluu@kien56.local>
2013-01-10 21:21:42 +0000

    added tone page

--- webapp/js/main.js
+++ webapp/js/main.js
@@ -1,1 +1,12 @@
+(function($){
+    $(document).ready(function(){
+        $('.start-button').click(function(){
+            $('.start-box').addClass('hidden');
+            $('.trainer-box').removeClass('hidden');
+        });
 
+        // Debug
+        $('.start-button').click();
+    });
+
+})(window.jQuery);
